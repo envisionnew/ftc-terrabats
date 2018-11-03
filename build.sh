c42072ffcc5d926089e4b5c636bf124a85d52c32
Rushil Srivastava <rushu0922@gmail.com>
2018-11-03 16:28:41 -0700

    remove licenes commands from build

--- build.sh
+++ build.sh
@@ -8,7 +8,3 @@ rm -rf ftc_app/.git
 
 rsync -a 2018/* ftc_app
 rm -rf ftc-terrabats
-
-sudo mkdir -p "$ANDROID_SDK/licenses"
-sudo echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_SDK/licenses/android-sdk-license"
-sudo echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_SDK/licenses/android-sdk-preview-license"
