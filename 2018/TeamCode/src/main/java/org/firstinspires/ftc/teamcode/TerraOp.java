7a62152cd167f2e9fdbf0c555d3024bf52faba81
Rushil Srivastava <rushu0922@gmail.com>
2018-11-02 19:18:33 -0700

    travis updates

---
+++ 2018/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TerraOp.java
@@ -0,0 +1,57 @@
+package org.terrabats.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "TerraOp: Test")
+public class TerraOp extends OpMode{
+    //All RobotParts
+    Terrabot robot = new Terrabot();
+
+
+
+
+
+
+    @Override
+    public void init() {
+
+        robot.init(hardwareMap);
+
+        telemetry.addData("", "Press Start");
+        telemetry.update();
+    }
+    @Override
+    public void init_loop(){
+    }
+    @Override
+    public void start() {
+    }
+    @Override
+    public void loop() {
+        if(gamepad1.left_bumper){
+            robot.out(0);
+        }
+        if(gamepad1.right_bumper) {
+            robot.out(180);
+        }
+
+        robot.intakeLift(gamepad2.right_stick_y);
+        robot.intake(gamepad2.left_stick_y);
+        robot.move(gamepad1.right_stick_y, gamepad1.left_stick_x);
+
+        if(gamepad1.left_trigger > 0) {
+            robot.lift(0.5);
+        }else if(gamepad1.right_trigger > 0) {
+            robot.lift(-0.5);
+        }else{
+            robot.lift(0);
+        }
+
+    }
+    @Override
+    public void stop() {
+    }
+
+}
---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir ftc_app
+git clone --depth=1 https://github.com/ftctechnh/ftc_app.git ftc_app
+rm -rf ftc_app/.git
+
+rsync -a ftc-terrabats/2018/* ftc_app
+rm -rf ftc-terrabats
