eebdbe3a5d95b8343910e4cafd517b19ef9a8bd3
Rushil Srivastava <rushu0922@gmail.com>
2018-11-02 19:23:43 -0700

    update build script

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ mkdir ftc_app
 git clone --depth=1 https://github.com/ftctechnh/ftc_app.git ftc_app
 rm -rf ftc_app/.git
 
-rsync -a ftc-terrabats/2018/* ftc_app
+rsync -a 2018/* ftc_app
 rm -rf ftc-terrabats
