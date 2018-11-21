9392ebb65ade76df2ee1426ffcc813bdd9c36c86
Saurish Srivastava <31828155+SaurishS@users.noreply.github.com>
2018-11-21 11:17:00 -0800

    Better comments

--- 2018/TeamCode/src/main/java/com/terrabats/ftc/teamcode/TerraOp.java
+++ 2018/TeamCode/src/main/java/com/terrabats/ftc/teamcode/TerraOp.java
@@ -6,16 +6,18 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "TerraOp: Test")
 public class TerraOp extends OpMode{
-    //All RobotParts
-    TerraBot robot = new TerraBot();
+
+    // Define a new "Terrabot" so that we can call functions. //
+
+    Terrabot robot = new Terrabot();
 
     @Override
     public void init() {
 
         robot.init(hardwareMap);
-
         telemetry.addData("", "Press Start");
         telemetry.update();
+
     }
     @Override
     public void init_loop(){
@@ -31,15 +33,17 @@ public class TerraOp extends OpMode{
         if(gamepad1.right_bumper) {
             robot.out(180);
         }
-
+        if(gamepad1.y){
+            robot.strafe();
+        }
         robot.intakeLift(gamepad2.right_stick_y);
         robot.intake(gamepad2.left_stick_y);
-        robot.move(gamepad1.right_stick_y, gamepad1.left_stick_x);
+        robot.move(-gamepad1.right_stick_y, -gamepad1.left_stick_x);
 
         if(gamepad1.left_trigger > 0) {
-            robot.lift(0.5);
+            robot.lift(gamepad1.left_trigger);
         }else if(gamepad1.right_trigger > 0) {
-            robot.lift(-0.5);
+            robot.lift(-gamepad1.right_trigger);
         }else{
             robot.lift(0);
         }
