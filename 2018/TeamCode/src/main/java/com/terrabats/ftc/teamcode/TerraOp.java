1a29b3c4ac385fceb4298ee515f21e5fc408b3b3
Saurish Srivastava <31828155+SaurishS@users.noreply.github.com>
2019-01-11 20:59:28 -0800

    Update TerraOp: Qualifier 2

--- 2018/TeamCode/src/main/java/com/terrabats/ftc/teamcode/TerraOp.java
+++ 2018/TeamCode/src/main/java/com/terrabats/ftc/teamcode/TerraOp.java
@@ -1,52 +1,91 @@
-package com.terrabats.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.ArrayList;
 
 
 @TeleOp(name = "TerraOp: Test")
 public class TerraOp extends OpMode{
-
-    // Define a new "Terrabot" so that we can call functions. //
-
-    TerraBot robot = new TerraBot();
+    //All RobotParts
+    Terrabot robot = new Terrabot();
+    boolean record = false;
+    ArrayList disRight = new ArrayList();
+    ArrayList disLeft = new ArrayList();
 
     @Override
     public void init() {
 
         robot.init(hardwareMap);
+
         telemetry.addData("", "Press Start");
         telemetry.update();
-
     }
     @Override
     public void init_loop(){
+
     }
     @Override
     public void start() {
+
     }
     @Override
     public void loop() {
-        if(gamepad1.left_bumper){
-            robot.out(0);
-        }
-        if(gamepad1.right_bumper) {
-            robot.out(180);
-        }
-        if(gamepad1.y){
-            robot.strafe();
-        }
-        robot.intakeLift(gamepad2.right_stick_y);
-        robot.intake(gamepad2.left_stick_y);
         robot.move(-gamepad1.right_stick_y, -gamepad1.left_stick_x);
 
-        if(gamepad1.left_trigger > 0) {
-            robot.lift(gamepad1.left_trigger);
-        }else if(gamepad1.right_trigger > 0) {
-            robot.lift(-gamepad1.right_trigger);
-        }else{
-            robot.lift(0);
+        if (gamepad1.a) {
+            robot.reset();
+            record = true;
         }
+
+        if(record){
+            if(robot.left.getCurrentPosition()>0||robot.right.getCurrentPosition()>0){
+                if(!robot.right.isBusy()&&!robot.left.isBusy()){
+                    disRight.add(robot.right.getCurrentPosition());
+                    disLeft.add(robot.left.getCurrentPosition());
+                    telemetry.addData("Recorded: R",robot.right.getCurrentPosition());
+                    telemetry.addData("Recorded: L",robot.left.getCurrentPosition());
+                    telemetry.update();
+                    robot.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    robot.right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                }
+            }
+
+
+        }
+
+        if (gamepad1.x){
+            record = false;
+        }
+
+        if (gamepad1.dpad_up){
+            int num = 0;
+            while(num < disRight.size()-1){
+                robot.apply(1*(int)(disLeft.get(num)),1*(int)(disRight.get(num)));
+                num += 1;
+            }
+            robot.left.setPower(0);
+            robot.left2.setPower(0);
+            robot.right.setPower(0);
+            robot.right2.setPower(0);
+        }
+
+        if (gamepad1.dpad_down){
+            int num = disRight.size()-1;
+            while(num<=0){
+                robot.apply(-1*(int)(disLeft.get(num)),-1*(int)(disRight.get(num)));
+                num -= 1;
+            }
+            robot.left.setPower(0);
+            robot.left2.setPower(0);
+            robot.right.setPower(0);
+            robot.right2.setPower(0);
+        }
+
+        robot.neverest.setPower(gamepad1.left_trigger);
+        robot.neverest.setPower(-gamepad1.right_trigger);
 
     }
     @Override
