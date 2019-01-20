34b76e12b0a4a3ab5b9a6167a5d638b304a61f0c
shuhul <31378175+shuhul@users.noreply.github.com>
2019-01-19 17:38:19 -0800

    Update TerraOp.java

--- 2018/TeamCode/src/main/java/com/terrabats/ftc/teamcode/TerraOp.java
+++ 2018/TeamCode/src/main/java/com/terrabats/ftc/teamcode/TerraOp.java
@@ -9,29 +9,36 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class TerraOpTest extends OpMode {
     TerraRunner bot = new TerraRunner();
     ElapsedTime time = new ElapsedTime();
-    private boolean record = false;
     @Override
     public void init() {
         bot.init(hardwareMap);
         bot.ext.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         bot.ext.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bot.arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bot.arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         time.reset();
     }
     @Override
     public void loop() {
-        if(((int)time.milliseconds()) % bot.DELAY_MS == 0){
-            bot.oldE = bot.ext.getCurrentPosition();
-        }
-
-
-        bot.move(-gamepad1.right_stick_y,gamepad1.left_stick_x);
+        bot.update();
+        bot.move(-gamepad1.right_stick_y,gamepad1.left_stick_x / 2);
         if(bot.isExtInLimits(gamepad2)) {
             bot.extend(-gamepad2.right_stick_y);
         }else {
             bot.extend(0);
         }
-
-        bot.lift(gamepad2.left_stick_y/2);
+        if(gamepad2.y) {
+            bot.dropMinerals();
+        }
+        if(bot.isDropping) {
+            if(gamepad2.left_stick_y < 0) {
+                bot.lift((gamepad2.left_stick_y / 2) + bot.restPower);
+            }else if(gamepad2.left_stick_y > 0){
+                bot.lift((gamepad2.left_stick_y) + bot.restPower);
+            }else{
+                bot.lift(+bot.restPower);
+            }
+        }
 
         if(gamepad2.right_trigger > 0) {
             bot.hang(-gamepad2.right_trigger);
@@ -44,14 +51,13 @@ public class TerraOpTest extends OpMode {
             bot.intake(1);
         }else if(gamepad2.left_bumper){
             bot.intake(-1);
-        }else{
+        }else {
             bot.intake(0);
         }
-        telemetry.addData("Pos",bot.ext.getCurrentPosition());
-        telemetry.addData("Limit", (5*bot.TICKS_PER_REVHD));
+        telemetry.addData("Angle: ", bot.getArmAngle());
+        telemetry.addData("Length: ", bot.ext.getCurrentPosition());
     }
     @Override
-    public void stop() {
-        record = false;
+    public void stop(){
     }
 }
