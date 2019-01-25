1460c06716ced050f64dfac8c78a1eddb763d549
Saurish Srivastava <31828155+SaurishS@users.noreply.github.com>
2019-01-25 13:20:46 -0800

    Update TerraOp.java

--- 2018/TeamCode/src/main/java/com/terrabats/ftc/teamcode/TerraOp.java
+++ 2018/TeamCode/src/main/java/com/terrabats/ftc/teamcode/TerraOp.java
@@ -3,99 +3,62 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
-import java.util.ArrayList;
-
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "TerraOp: Test")
-public class TerraOp extends OpMode{
-    //All RobotParts
-    Terrabot robot = new Terrabot();
-    boolean record = false;
-    ArrayList disRight = new ArrayList();
-    ArrayList disLeft = new ArrayList();
-
+public class TerraOp extends OpMode {
+    TerraRunner bot = new TerraRunner();
+    ElapsedTime time = new ElapsedTime();
     @Override
     public void init() {
-
-        robot.init(hardwareMap);
-
-        telemetry.addData("", "Press Start");
-        telemetry.update();
-    }
-    @Override
-    public void init_loop(){
-
-    }
-    @Override
-    public void start() {
-
+        bot.init(hardwareMap);
+        bot.ext.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bot.ext.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bot.arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bot.arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        time.reset();
     }
     @Override
     public void loop() {
-        robot.move(-gamepad1.right_stick_y, -gamepad1.left_stick_x);
-
-        if (gamepad1.a) {
-            robot.reset();
-            record = true;
+        bot.update();
+        bot.move(-gamepad1.right_stick_y,gamepad1.left_stick_x / 2);
+        if(bot.isExtInLimits(gamepad2)) {
+            bot.extend(-gamepad2.right_stick_y);
+        }else {
+            bot.extend(0);
         }
-
-        if(record){
-            if(robot.left.getCurrentPosition()>0||robot.right.getCurrentPosition()>0){
-                if(!robot.right.isBusy()&&!robot.left.isBusy()){
-                    disRight.add(robot.right.getCurrentPosition());
-                    disLeft.add(robot.left.getCurrentPosition());
-                    telemetry.addData("Recorded: R",robot.right.getCurrentPosition());
-                    telemetry.addData("Recorded: L",robot.left.getCurrentPosition());
-                    telemetry.update();
-                    robot.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    robot.right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                }
+        if(gamepad2.y) {
+            bot.dropMinerals();
+        }
+        if(bot.isDropping) {
+            if(gamepad2.left_stick_y < 0) {
+                bot.lift((gamepad2.left_stick_y / 2) + bot.restPower);
+            }else if(gamepad2.left_stick_y > 0){
+                bot.lift((gamepad2.left_stick_y) + bot.restPower);
+            }else{
+                bot.lift(+bot.restPower);
             }
-
-
         }
 
-        if (gamepad1.x){
-            record = false;
+        if(gamepad1.right_trigger > 0) {
+            bot.hang(-gamepad1.right_trigger);
+        }else if(gamepad1.left_trigger > 0){
+            bot.hang(gamepad1.left_trigger);
+        }else{
+            bot.hang(0);
         }
-
-        if (gamepad1.dpad_up){
-            int num = 0;
-            while(num < disRight.size()-1){
-                robot.apply(1*(int)(disLeft.get(num)),1*(int)(disRight.get(num)));
-                num += 1;
-            }
-            robot.left.setPower(0);
-            robot.left2.setPower(0);
-            robot.right.setPower(0);
-            robot.right2.setPower(0);
+        if(gamepad2.right_bumper){
+            bot.intake(1);
+        }else if(gamepad2.left_bumper){
+            bot.intake(-1);
+        }else {
+            bot.intake(0);
         }
-
-        if (gamepad1.dpad_down){
-            int num = disRight.size()-1;
-            while(num<=0){
-                robot.apply(-1*(int)(disLeft.get(num)),-1*(int)(disRight.get(num)));
-                num -= 1;
-            }
-            robot.left.setPower(0);
-            robot.left2.setPower(0);
-            robot.right.setPower(0);
-            robot.right2.setPower(0);
-        }
-
-        robot.neverest.setPower(gamepad1.left_trigger);
-        robot.neverest.setPower(-gamepad1.right_trigger);
-
-        robot.al.setPower(gamepad2.left_trigger);
-        robot.al.setPower(-gamepad2.right_trigger);
-
-        robot.ar.setPower(gamepad2.left_stick_x);
-        robot.ar.setPower(-gamepad2.left_stick_y);
-
+        telemetry.addData("Angle: ", bot.getArmAngle());
+        telemetry.addData("Length: ", bot.ext.getCurrentPosition());
     }
     @Override
-    public void stop() {
+    public void stop(){
     }
-
 }
+
